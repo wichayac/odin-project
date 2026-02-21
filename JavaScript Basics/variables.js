39d628ef07295cfe06b142dc9f818c0fe8392f1a
wichayac <wichaya.c@kkumail.com>
2026-02-21 21:34:24 +0700

    js variables

---
+++ JavaScript Basics/variables.js
@@ -0,0 +1,28 @@
+let firstName = "Wichaya";
+let lastName = "Chapinjai";
+
+console.log(firstName);
+console.log(lastName);
+
+let age = 20;
+console.log(age);
+
+age = 21;
+console.log(age);
+
+/* the output should be 
+Wichaya
+Chapinjai
+20
+21 */
+
+const pi = 22/7;
+console.log(pi);
+
+/* pi = 10;
+console.log(pi); error! 
+let, can re-assign.
+const can’t re-assign */
+
+let number = (3 + 2) - 76 * (1 + 1) //PEMDAS(parenthesis>exponential>multiply/division>add/subtract)
+console.log(number);
